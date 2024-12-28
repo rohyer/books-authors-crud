cd17f23b1632da81ff30736381c8fc7e04a33349
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-12-28 11:26:02 -0300

    Create route to home

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Books from "./pages/Books";
 import Authors from "./pages/Authors";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
 
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="livros" element={<Books />} />
         <Route path="autores" element={<Authors />} />
       </Routes>
