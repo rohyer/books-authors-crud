f0338402e50e32f223539ba4b89222491e046b72
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-12-30 18:16:56 -0300

    Create context

---
+++ src/contexts/AuthorsBooksContext.jsx
@@ -0,0 +1,51 @@
+import { createContext, useContext, useState } from "react";
+
+export const AuthorsBooksContext = createContext();
+
+export const AuthorsBooksProvider = ({ children }) => {
+  const [authors, setAuthors] = useState([]);
+  const [books, setBooks] = useState([]);
+
+  const addAuthorToState = (newAuthor) => {
+    setAuthors((prevAuthors) => [...prevAuthors, newAuthor]);
+  };
+
+  const addAllAuthorsToState = (authors) => {
+    setAuthors(authors);
+  };
+
+  const deleteAuthorFromState = (id) => {
+    setAuthors((prevAuthors) =>
+      prevAuthors.filter((author) => author.id !== id)
+    );
+  };
+
+  const addBookToState = (newBook) => {
+    setBooks((prevBooks) => [...prevBooks, newBook]);
+  };
+
+  const addAllBooksToState = (books) => {
+    setBooks(books);
+  };
+
+  const deleteBookFromState = (id) => {
+    setBooks((prevBooks) => prevBooks.filter((book) => book.id !== id));
+  };
+
+  return (
+    <AuthorsBooksContext.Provider
+      value={{
+        authors,
+        books,
+        addAuthorToState,
+        addAllAuthorsToState,
+        deleteAuthorFromState,
+        addBookToState,
+        addAllBooksToState,
+        deleteBookFromState
+      }}
+    >
+      {children}
+    </AuthorsBooksContext.Provider>
+  );
+};
