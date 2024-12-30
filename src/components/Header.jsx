9f3a15bba573329c6652e9fa58d4a42caac44913
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-12-29 23:49:03 -0300

    Active link menu when route change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-import * as Dialog from "@radix-ui/react-dialog";
+import { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const StyledHeader = styled.header`
   display: flex;
@@ -26,45 +26,7 @@ const Nav = styled.nav`
       position: relative;
       list-style: none;
       margin: 0 1rem;
-      padding: 1rem 0;
       transition: all 0.25s;
-
-      &:hover {
-        & > a {
-          color: #333;
-        }
-
-        &:after {
-          width: 100%;
-        }
-
-        /* & > ul {
-          opacity: 1;
-          pointer-events: all;
-        } */
-      }
-
-      &:after {
-        content: "";
-        position: absolute;
-        bottom: 0px;
-        left: 0;
-        right: 0;
-        width: 0%;
-        height: 4px;
-        background-color: #0073d0;
-        border-radius: 6px;
-        margin: auto;
-        transition: all 0.2s;
-      }
-
-      /* &:nth-child(4) {
-        &:hover {
-          &:after {
-            width: 140px;
-          }
-        }
-      } */
 
       & > a {
         position: relative;
@@ -73,25 +35,82 @@ const Nav = styled.nav`
         font-style: normal;
         font-weight: 600;
         color: #707070;
+        padding: 1rem 0;
         transition: all 0.25s;
+
+        &:hover {
+          color: #333;
+
+          &:after {
+            width: 100%;
+          }
+        }
+
+        &.activeLink {
+          color: #333;
+
+          &:before {
+            content: "";
+            position: absolute;
+            bottom: 0px;
+            left: 0;
+            right: 0;
+            width: 100%;
+            height: 4px;
+            background-color: #0073d0;
+            border-radius: 6px;
+            margin: auto;
+            transition: all 0.2s;
+          }
+        }
+
+        &:after {
+          content: "";
+          position: absolute;
+          bottom: 0px;
+          left: 0;
+          right: 0;
+          width: 0%;
+          height: 4px;
+          background-color: #0073d0;
+          border-radius: 6px;
+          margin: auto;
+          transition: all 0.2s;
+        }
       }
     }
   }
 `;
 
 function Header() {
+  const location = useLocation();
+
+  const isActive = (path) => location.pathname === path;
+
   return (
     <StyledHeader>
       <Nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" className={isActive("/") ? "activeLink" : undefined}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="livros">Livros</Link>
+            <Link
+              to="livros"
+              className={isActive("/livros") ? "activeLink" : undefined}
+            >
+              Livros
+            </Link>
           </li>
           <li>
-            <Link to="autores">Autores</Link>
+            <Link
+              to="autores"
+              className={isActive("/autores") ? "activeLink" : undefined}
+            >
+              Autores
+            </Link>
           </li>
         </ul>
       </Nav>
